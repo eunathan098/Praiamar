9ca913c8ac25859c5e439cc55686588c10670287
Nathan <145821197+eunathan098@users.noreply.github.com>
2024-06-13 23:45:16 -0300

    Adição de Scroll e Javascript á página..

---
+++ script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.header-btn', {
+    rotate: {x: 0, y: 80, z:0 },
+    duration:2000
+ });
+
+sr.reveal('.galeria-img', {duration: 1000 });
+
+sr.reveal('.header-btn2', {duration: 1000 });
